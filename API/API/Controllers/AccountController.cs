c6ef922ab7ea1caca51b51d7023bd136af3b29d8
Aljamil1113 <aarazasaljamil@gmail.com>
2023-02-27 22:15:55 +0800

    Backend: Login, Register and GetCurrentUser

--- API/API/Controllers/AccountController.cs
+++ API/API/Controllers/AccountController.cs
@@ -1,9 +1,73 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using API.Dtos;
+using API.Entities;
+using API.Service;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
     public class AccountController : BaseApiController
     {
+        private readonly UserManager<User> userManager;
+        private readonly TokenService tokenService;
+
+        public AccountController(UserManager<User> _userManager, TokenService _tokenService)
+        {
+            userManager = _userManager;
+            tokenService = _tokenService;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await userManager.FindByNameAsync(loginDto.Username);
+            if (user == null || !await userManager.CheckPasswordAsync(user, loginDto.Password))
+            {
+                return Unauthorized();
+            }
+            return new UserDto
+            {
+                Email = user.Email,
+                Token = await tokenService.GenerateToken(user)
+            };
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> Register(RegisterDto registerDto)
+        {
+            var user = new User { UserName = registerDto.Username, Email = registerDto.Email };
+
+            var result = await userManager.CreateAsync(user, registerDto.Password);
+
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+
+                return ValidationProblem();
+            }
+
+            await userManager.AddToRoleAsync(user, "Member");
+
+            return StatusCode(201);
+        }
+
+        [Authorize]
+        [HttpGet("currentUser")]
+        public async Task<ActionResult<UserDto>> GetCurrentUser()
+        {
+            var user = await userManager.FindByNameAsync(User.Identity.Name);
+
+            return new UserDto
+            {
+                Email = user.Email,
+                Token = await tokenService.GenerateToken(user)
+            };
+
+        }
     }
 }
